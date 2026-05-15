e6deadc074c4e20f5e5094e9cb06c2cddffb0a65
Sherlin Ticsay <sherlinticsay06@gmail.com>
2026-05-15 11:25:53 +0800

    Added functions per arithmentic operator

---
+++ simple_calcuator.py
@@ -0,0 +1,16 @@
+# simple calculator that handle arithmetic operations to showcase collaboration using git and Github
+
+# Functions for operations
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "Error: Division by zero"
